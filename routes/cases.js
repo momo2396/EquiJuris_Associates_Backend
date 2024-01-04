3938b748361057a289e851028bbc58e536255440
momo2396 <foyazunnesamomo.345@gmail.com>
2024-01-04 14:55:11 +0600

    single-case api  created

--- routes/cases.js
+++ routes/cases.js
@@ -4,6 +4,7 @@ const router = express.Router()
 // functions import
 const roleCheck = require('../functions/roleCheck')
 const { caseCollection } = require('../functions/databaseClient')
+const { ObjectId } = require('mongodb')
 
 
 router.get('/', async (req, res) => {
@@ -14,11 +15,18 @@ router.get('/', async (req, res) => {
 router.get('/my-cases', async (req, res) => {
   let email = req.query.email
   let cases = await caseCollection.find({lawyerEmail: email}).toArray()
-  // console.log(+new Date(cases[1]?.createdAt))
   let sortedCases = cases.sort((a,b) => +new Date(b?.createdAt)  - +new Date(a?.createdAt) )
   res.send({ data: sortedCases, success: true, message: "" })
 })
 
+router.get('/single-case/:id', async (req, res) => {
+  let id = req.params.id
+  let query = {
+      _id  : new ObjectId(id)
+  }
+  let singleCase = await caseCollection.findOne(query)
+  res.send({ data: singleCase, success: true, message: "" })
+})
 
 router.post("/insert-case", async (req, res) => {
     let newCase = req.body;
